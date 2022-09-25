dacd6d39f66951e391606418c70a8b7de7d349e4
Zhao Yongzhen <icstorm2020@outlook.com>
2022-09-25 11:47:09 +0800

    feat: generate furigana

--- extension.js
+++ extension.js
@@ -1,5 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+const KuromojiAnalyzer = require("kuroshiro-analyzer-kuromoji");
+const Kuroshiro = require("kuroshiro")
+
 const vscode = require('vscode');
 
 // this method is called when your extension is activated
@@ -10,25 +13,31 @@ const vscode = require('vscode');
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "furigana-generator" is now active!');
+	const kuroshiro = new Kuroshiro();
+	kuroshiro.init(new KuromojiAnalyzer())
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('furigana-generator.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand('furigana-generator.gen-furigana', function () {
+		const editor = vscode.window.activeTextEditor;
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from furigana-generator!');
+		if (editor) {
+			const document = editor.document;
+			const selection = editor.selection;
+
+			const sentence = document.getText(selection);
+
+			kuroshiro.convert(sentence, { to: "hiragana", mode: "okurigana" })
+				.then(function (result) {
+					editor.edit(editBuilder => {
+						editBuilder.replace(selection, result);
+					});
+				})
+		}
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
